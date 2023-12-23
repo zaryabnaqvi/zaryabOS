514e98b46131a7e349592fce143e11a2bfe712c2
A-Rafay32 <abdulrafaychohan986@gmail.com>
2023-12-23 20:10:43 +0500

    Part 2 ( Rafay )

--- loader.s
+++ loader.s
@@ -11,7 +11,21 @@ global loader                   ; the entry symbol for ELF
         dd FLAGS                    ; the flags,
         dd CHECKSUM                 ; and the checksum
 
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+
+    section .bss
+    align 4                         ; align at 4 bytes
+    kernel_stack:                   ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
+    
+
     loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                                    ; stack (end of memory area)
+        external sum_of_three ; the function sum_of_three is defined elsewhere
+push dword 3 ; arg3
+push dword 2 ; arg2
+push dword 1 ; arg1
+call sum_of_three ; call the function, the result will be in eax                                                    
     .loop:
         jmp .loop                   ; loop forever
